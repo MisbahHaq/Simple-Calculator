ac08eff1b832b6baa00341eb163b027bd4f2b1e2
Misbah Haque <misbahmuneer2003@gmail.com>
2025-06-13 16:20:00 +0500

    Update

--- Miscellaneous/script.js
+++ Miscellaneous/script.js
@@ -1,16 +1,18 @@
 var istatus = document.querySelector("h5")
 
-var addFriend = document.querySelector("#add")
-var check = 0;
+var btn = document.querySelector("#add")
+var check = 0
 
-addFriend.addEventListener("click", function () {
+btn.addEventListener("click", function () {
     if (check == 0) {
         istatus.innerHTML = "Friends"
         istatus.style.color = "green"
+        btn.innerHTML = "Remove Friend"
         check = 1;
     } else {
         istatus.innerHTML = "Stranger"
         istatus.style.color = "red"
+        btn.innerHTML = "Add Friend"
         check = 0;
     }
 })
