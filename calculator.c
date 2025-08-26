1763a3a158c4cf5b3a8e051f64316c06e2065352
Misbah Haque <misbahmuneer2003@gmail.com>
2025-08-26 10:42:58 +0500

    Update

---
+++ calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n Welcome to Simple Calculator\n");
+    printf("\n Choose one of the following options:");
+    printf("\n1. Add");
+    printf("\n2. Subtract");
+    printf("\n3. Multiply");
+    printf("\n4. Divide");
+    printf("\n5. Modulus");
+    printf("\n6. Power");
+    printf("\n7. Exit");
+
+    return 0;
+}
